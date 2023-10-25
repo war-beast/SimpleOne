12ee00b70c433dda091f4c20df4556217f2d1afb
Vladimir Sharapov <v.sharapov@artlogics.ru>
2023-10-25 15:06:11 +0300

    SO-1 Добавил интерфейс и загрушку стратегии сортировки расчёской

---
+++ SimpleAlgo/Services/CombSortStrategy.cs
@@ -0,0 +1,11 @@
+﻿using SimpleAlgo.Interfaces;
+
+namespace SimpleAlgo.Services;
+
+public class CombSortStrategy<T> : SortingStrategyBase<T>, ICombSortStrategy<T> where T : IComparable<T>
+{
+	public override T[] Sort(T[] array)
+	{
+		throw new NotImplementedException();
+	}
+}
