7575baf3ede8b26ab9ed053d5a23e3daa9dfb8ba
Vladimir Sharapov <v.sharapov@artlogics.ru>
2023-10-27 15:31:00 +0300

    SO-4 Добавил интерфейс и заглушку реализации сортировки слиянием

---
+++ SimpleAlgo/Services/MergeSortingStrategy.cs
@@ -0,0 +1,11 @@
+﻿using SimpleAlgo.Interfaces;
+
+namespace SimpleAlgo.Services;
+
+public class MergeSortingStrategy<T> : SortingStrategyBase<T>, IMergeSortingStrategy<T> where T : IComparable<T>
+{
+	public override T[] Sort(T[] array)
+	{
+		throw new NotImplementedException();
+	}
+}
